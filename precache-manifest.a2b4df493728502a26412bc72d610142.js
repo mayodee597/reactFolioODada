df67679b6e9de14043fa26f7d51b92ffae3f0ab5
mayodee597 <mayodee13@gmail.com>
2020-10-17 09:33:25 -0400

    Updates

---
+++ precache-manifest.a2b4df493728502a26412bc72d610142.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc44831313d94a98cfa85085b54c3247",
+    "url": "/reactFolioODada/index.html"
+  },
+  {
+    "revision": "7373b375908ecd0a3c6b",
+    "url": "/reactFolioODada/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "1b7534f6a07ba40dfa30",
+    "url": "/reactFolioODada/static/css/main.b2ddf829.chunk.css"
+  },
+  {
+    "revision": "7373b375908ecd0a3c6b",
+    "url": "/reactFolioODada/static/js/2.126b2ccd.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/reactFolioODada/static/js/2.126b2ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b7534f6a07ba40dfa30",
+    "url": "/reactFolioODada/static/js/main.ee8f7085.chunk.js"
+  },
+  {
+    "revision": "6190edfef8e67fe666bd",
+    "url": "/reactFolioODada/static/js/runtime-main.66e4a4aa.js"
+  }
+]);
